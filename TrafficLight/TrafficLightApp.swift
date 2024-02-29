cb88f72556ca132b77b3dcfdb78eef59af41b1f6
Varya Utkina <varya2412@mail.ru>
2024-02-29 21:48:40 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Варвара Уткина on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
